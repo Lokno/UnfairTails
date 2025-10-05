8018ba7f5ef085f9c0ec16c8d4fb849eee523f6a
Lokno <lokno1@gmail.com>
2025-10-04 22:08:54 -0400

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace UnfairTails;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        HarmonyLib.Harmony harmony = new (MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(MessageManager), "GetVisibleMessages")]
+    public class MessageManager_GetVisibleMessages_Patch
+    {
+        private static void Postfix(ref string[] __result)
+        {
+            for (int j = 0; j < __result.Length; j++)
+            {
+                __result[j] = __result[j].Replace("tails", "temp").Replace("Tails", "Temp").Replace("TAILS", "TEMP");
+                __result[j] = __result[j].Replace("heads", "tails").Replace("Heads", "Tails").Replace("HEADS", "TAILS");
+                __result[j] = __result[j].Replace("temp", "heads").Replace("Temp", "Heads").Replace("TEMP", "HEADS");
+            }
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(GenericMessage), "SetMessage")]
+    public class GenericMessage_SetMessage_Patch
+    {
+        private static void Prefix(ref string message)
+        {
+            message = message.Replace("tails", "temp").Replace("Tails", "Temp").Replace("TAILS", "TEMP");
+            message = message.Replace("heads", "tails").Replace("Heads", "Tails").Replace("HEADS", "TAILS");
+            message = message.Replace("temp", "heads").Replace("Temp", "Heads").Replace("TEMP", "HEADS");
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(HeadsChanceCounter), "Update")]
+    public class HeadsChanceCounter_Update_Patch
+    {
+        private static void Postfix(HeadsChanceCounter __instance)
+        {
+            var textField = (TMPro.TMP_Text)AccessTools.Field(typeof(HeadsChanceCounter), "text").GetValue(__instance);
+            textField.text = textField.text.Replace("TAILS", "TEMP").Replace("HEADS", "TAILS").Replace("TEMP", "HEADS");
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(FlipResultMessage), "ShowResult")]
+    public class FlipResultMessagePatch
+    {
+        private static void Postfix(FlipResultMessage __instance)
+        {
+            var textField = (TMPro.TMP_Text)AccessTools.Field(typeof(FlipResultMessage), "text").GetValue(__instance);
+            textField.text = textField.text.Replace("TAILS", "TEMP").Replace("HEADS", "TAILS").Replace("TEMP", "HEADS");
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(ShopButton), "Update")]
+    public class ShopButtonPatch
+    {
+        private static void Postfix(ShopButton __instance)
+        {
+            var textField = (TMPro.TMP_Text)AccessTools.Field(typeof(ShopButton), "text").GetValue(__instance);
+            textField.text = textField.text.Replace("TAILS", "TEMP").Replace("HEADS", "TAILS").Replace("TEMP", "HEADS");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Unfair Tails
+
+Mod for the game Unfair Flips that changes the objective from 10 heads in a row to 10 tails in a row.
+
+It simply swaps all instances of the text 'heads' with 'tails' and vice versa. Swaps text back for save data so save shouldn't be impacted (message history).
+
+### Manual Installation
+
+1.) Download BepInEx (https://github.com/BepInEx/BepInEx/releases/tag/v5.4.23.4)
+    Download and compile this mod (Precompiled dll coming soon)
+
+2.) Extract the contents of the zip archive into the EasyDeliveryCo game directory (default: "C:\Program Files (x86)\Steam\steamapps\common\Unfair Flips")
+
+3.) Start the game and close it again. This will do the first time set up for Bepinex.
+
+4.) Download the mod from Thunderstore (https://thunderstore.io/c/peak/p/Lokno/UnfairTrails/)
+
+5.) Unzip the zip archive into ...\Unfair Flips\BepInEx\plugins
+
+6.) Run the game
+
